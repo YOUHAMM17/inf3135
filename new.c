580a9f77a39c033a586a4846762cb36fa30f4517
YOUHAMW17 <ec091334@ens.uqam.ca>
2025-07-14 15:51:26 -0400

    Deboggage foncionnel

---
+++ new.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // printf("Hello, world..!\n");
+     struct student {
+          char name[50];
+          int age;
+          float gpa;
+     };
+     struct student s1 = {"John", 20, 3.5};
+     printf("%s\n", s1.name);        
+     return 0;
+}
