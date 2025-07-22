954e0dd9998ce6b7bc5149f42c50aed54fc1978a
YOUHAMW17 <ec091334@ens.uqam.ca>
2025-07-22 10:03:31 -0400

    Refactor!

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
+
+// Ensure no conflicting printf declaration exists
 int main(int argc, char *argv[]) {
+    //variables
+    char name[10]="";
+    int age=0;
+    char bloodType='\0';
+    //input/output
+    printf("Hello\nwhat is your name?\n");     
+    scanf("%s",name);
+    printf("Hello.. %s\n",name);
+
+    printf("How old are you?\n");
+    scanf("%d",&age);
+    printf("You are %d years old\n",age);
+
+    printf("how tall are you?\n");
+    float height=0.0;
+    scanf("%f",&height);
+    printf("You are %.2f meters tall\n",height);
+
+    printf("what your blood type?\n");
    
-   
-    printf("Hello, welcome to C programming!\n");     
+    scanf(" %c", &bloodType);
+    printf("your blood type is %c\n",bloodType);
     
     return 0; // Good practice to return a value from main
 }
