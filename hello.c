594553ee4814669e69fd747668baa6781d0e7fa2
YOUHAMW17 <ec091334@ens.uqam.ca>
2025-09-08 12:54:00 -0400

    test login

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 // hello.c - Premier programme en C
 
 #include <stdio.h>  // Bibliothèque pour afficher du texte
+#include <stddef.h> // Bibliothèque pour la fonction sizeof
 
 int main(void) {
     printf("Hello, world.\n"); // Affiche un message suivi d'un saut de ligne
@@ -13,5 +14,5 @@ int length = sizeof(tab) / sizeof(tab[0]);  // → 10 éléments
 printf("sizeof(tab) = %d\n", s); // Affiche la taille de l'array en octets
     printf("sizeof(tab[0]) = %d\n", sizeof(tab[0])); // Affiche la taille d'un élément de l'array en octets
     printf("length = %ld\n", length);  // Affiche le nombre d'éléments dans l'array
-    return 0; // Indique que le programme s'est terminé avec succès
+    return 0; // Indique que le programme s'est terminé avec succès.
 }
